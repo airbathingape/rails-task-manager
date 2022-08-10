7affea3486f4e9be8b5c35a0a85e23e4fe322107
Daniel Domingo <domingo.daniel21@gmail.com>
2022-08-10 20:27:00 +0900

    fixed spacing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,11 +33,11 @@ class TasksController < ApplicationController
     end
   end
 
-      def destroy
-        @task = Task.find(params[:id])
-        @task.destroy
-        redirect_to tasks_path, status: :see_other
-      end
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
 
   private
 
